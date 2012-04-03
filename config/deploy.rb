0aabf619f67008639768aff23fee549808f889a4
Romain Dardour <romain@unity.fr>
2012-04-03 11:03:28 +0200

    Add Capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,1 +1,10 @@
 require 'bundler/capistrano'
+
+set :repository, "git@github.com:sixdegrees/oahu-website.git"
+set :scm, :git
+set :use_sudo, false
+set :deploy_via, :remote_cache
+set :keep_releases, 4
+
+set :flowdock_project_name, "Oahu Website"
+set :flowdock_deploy_tags, ["oahu"] 
